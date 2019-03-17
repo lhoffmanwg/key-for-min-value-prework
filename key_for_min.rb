95290205e1d9a014b48ec9c5d37b0066c003a54c
Laura Hoffman <lhoffmanwg@gmail.com>
2019-03-17 03:30:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_name = nil
   lowest_age = 1000000
-  name_hash.collect do |name, age|
+  name_hash.each do |name, age|
     if age < lowest_age 
       lowest_age = age
       lowest_name = name
